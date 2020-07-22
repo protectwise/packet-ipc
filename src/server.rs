dae894eab13602c170724b135fbd2ee2c056d454
ben kornmeier <ben@protectwise.com>
2020-07-22 10:38:54 -0600

    oof not sure if we want to include this code now that it has a mutex in it... maybe a seperate type of connection?

--- src/server.rs
+++ src/server.rs
@@ -3,6 +3,7 @@ use crate::errors::Error;
 use crate::packet::{AsIpcPacket, IpcPacket};
 use ipc_channel::ipc::{IpcOneShotServer, IpcSender};
 use log::*;
+use std::sync::{Arc, Mutex};
 
 pub type SenderMessage = Option<Vec<IpcPacket>>;
 pub type Sender = IpcSender<SenderMessage>;
@@ -31,25 +32,40 @@ impl Server {
 
         info!("Accepted connection from {:?}", tx);
 
+        let tx = Arc::new(Mutex::new(tx));
+
         Ok(ConnectedIpc { connection: tx })
     }
 }
 
+
 pub struct ConnectedIpc {
-    connection: Sender,
+    connection: Arc<Mutex<Sender>>,
 }
 
 impl ConnectedIpc {
-    pub fn send<T: AsIpcPacket>(&self, packets: Vec<T>) -> Result<(), Error> {
+    pub async fn send<T: AsIpcPacket>(&self, packets:Vec<T>) -> Result<(), Error> {
         let ipc_packets: Vec<_> = packets.iter().map(IpcPacket::from).collect();
-        self.connection.send(Some(ipc_packets)).map_err(|e| {
-            error!("Failed to send {:?}", e);
-            Error::Bincode(e)
-        })
+        Self::internal_send(Arc::clone(&self.connection), ipc_packets).await
+    }
+
+    async fn internal_send(sender: Arc<Mutex<Sender>>, ipc_packets: Vec<IpcPacket>) -> Result<(), Error> {
+        blocking::Unblock::new(()).with_mut(move |_| {
+            let sender = Arc::clone(&sender);
+            let sender = sender.lock().map_err(|_| Error::Mutex())?;
+            sender.send(Some(ipc_packets)).map_err(|e| {
+                error!("Failed to send {:?}", e);
+                Error::Bincode(e)
+            });
+            Ok(())
+        }).await
+
     }
 
     pub fn close(&mut self) -> Result<(), Error> {
-        self.connection.send(None).map_err(Error::Bincode)?;
+        let connection = Arc::clone(&self.connection);
+        let connection = connection.lock().map_err(|_| Error::Mutex())?;
+        connection.send(None).map_err(Error::Bincode)?;
         Ok(())
     }
 }
